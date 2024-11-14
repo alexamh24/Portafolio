767f94b9cb63acda4f1cd0e113fa9728cc059762
Alexa <22690337@tecvalles.mx>
2024-11-13 20:33:07 -0600

    cambios 13/11

--- js/eventos.js
+++ js/eventos.js
@@ -11,21 +11,48 @@ otro_boton.addEventListener("click",
         salida.innerText = "Otro saludo 🌸";
 });
 
-otro_boton.addEventListener("mouseenter", function(){
-    this.innerText = " Entró el mouse";
+otro_boton.addEventListener("mouseenter", 
+    function(){
+    this.innerText = "Entró el mouse";
 });
 
-otro_boton.addEventListener("mouseleave", function(){
+otro_boton.addEventListener("mouseleave",
+    function(){
     this.innerText = " Salió el mouse";
 });
 
-otro_boton.addEventListener("mouseover", function(){
-    this.innerText = " 🐭";
+otro_boton.addEventListener("mousemove",
+    function(){
+    this.innerText = " 🐭 ";
 });
 
 let nombreText = document.getElementById("nombre");
 let salida_teclado = document.getElementById("salida_teclado");
 
-nombreText.addEventListener("keydown", function(event){
-    salida_teclado.innerHTML = `Se presionó <kbd>${event.key}</kbd>`;
+nombreText.addEventListener("keydown",
+    function(event){
+    salida_teclado.innerText = `Se presionó ${event.key}`; // <kbd>   </kbd>
+});
+
+
+// let formulario = document.querySelector("form");
+// formulario.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     console.log("Formulario enviado");
+//     alert("Se intento procesar el formulario");
+// });
+
+let miform = document.getElementById("miform");
+miform.addEventListener("submit",
+    function(event) {
+    event.preventDefault();
+    let atxt = document.getElementById("a");
+    let btxt = document.getElementById("b");
+    // console.log(a,b);
+    //     alert("Se intento procesar el formulario");
+    let salida = document.getElementById("salida_suma");
+    let a = parseInt(atxt.value);
+    let b = parseInt(btxt.value);
+    let resultado = a + b;
+    salida.innerText = resultado;
 });
---
+++ js/objetos2.js
@@ -0,0 +1,64 @@
+// let nombre = prompt("¿Cómo te llamas?");
+// if(nombre){
+//     alert(`Hola ${nombre}`);
+// } else{
+// alert("Anónimo");
+// }
+
+let lista= document.getElementById("log");
+
+function registro(texto){
+    let item = document.createElement("li");
+    item.innerText = texto;
+    return item;
+}
+
+lista.appendChild(registro("DOM LOADED!"));
+
+let timer = setTimeout(() =>{
+    lista.appendChild(registro("Timer a los 3 seg."));
+}, 3000);
+
+
+let repetidor = setInterval(() => {
+    lista.appendChild(registro("Cada 1.5 seg. ⌛"));
+}, 1500);
+
+let activaTimerBtn = document.getElementById("activaTimer");
+let stopBtn = document.getElementById("pararRepetidor");
+
+activaTimerBtn.addEventListener("click", function(){
+    lista.appendChild(registro("Inicia timer..."));
+    timer.setTimeout(3000);
+    clearTimeout(timer);
+});
+
+stopBtn.addEventListener("click", function(){
+    lista.appendChild(registro("Parar repetidor..."));
+    clearInterval(repetidor);
+});
+
+console.table(location);
+let secure =  document.getElementById("secure");
+if(location.protocol.value == "https"){
+    secure.innerText = "Seguro";
+}else{
+    secure.innerText = "No seguro";
+}
+
+let fecha_actual = new Date();
+
+console.log(fecha_actual.getFullYear());
+console.log(fecha_actual.getMonth());
+console.log(fecha_actual.getDate());
+console.log(fecha_actual.getHours());
+console.log(fecha_actual.getMinutes());
+console.log(fecha_actual.getSeconds());
+
+let navidad2024 = new Date("2024-12-24 12:00:00");
+let fin2024 = new Date(2024, 11, 31, 23, 59, 59);
+
+console.log(navidad2024);
+console.log(fin2024);
+
+console.table(navigator);
