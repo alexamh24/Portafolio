6bf7b4ec1271eb01e16f9c31529feb0d0efe1fba
Alexa <22690337@tecvalles.mx>
2024-11-15 16:16:06 -0600

    act 15-11-24

---
+++ js/poke_api.js
@@ -0,0 +1,23 @@
+let nombreTxt = document.getElementById("pokemon-name")
+let typesList = document.getElementById("pokemon-types")
+let image = document.getElementById("pokemon-image")
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then((response) => response.json())
+    .then((pokemon) =>{
+        console.log(pokemon.name)
+        console.log(pokemon.types[0].type.name)
+        console.log(pokemon.sprites.front_default)
+        nombreTxt.innerText = pokemon.name
+        let lista = document.createElement("ul")
+        typesList.appendChild(lista)
+        pokemon.types.forEach(element =>{
+            let nombre = element.type.name
+            let li = document.createElement("li")
+            
+            li.innerHTML = nombre
+            lista.appendChild(li)
+        })
+        image.setAttribute("src",pokemon.sprites.front_default)
+    })
+    .catch((error) => console.error(error))
